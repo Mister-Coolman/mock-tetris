01dd1d72396c53a8ac878b364d95c98f08fbf555
Oscar Yeung <111371967+Oscar3334@users.noreply.github.com>
2024-05-29 13:52:26 -0400

    Create tetrisGame.py

---
+++ tetrisGame.py
@@ -0,0 +1,16 @@
+import pygame
+from board import *
+from tetramino import *
+import configs
+import random
+
+"""
+This will be the object created for an instance of the game
+"""
+
+class Tetris:
+    def __init__(self, x: int, y: int) -> None:
+        this.x = x
+        this.y = y
+        
+    
