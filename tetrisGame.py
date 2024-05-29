92a19fdfaa6ce250bb951fe3dc877f173a15f810
Oscar Yeung <111371967+Oscar3334@users.noreply.github.com>
2024-05-29 13:58:50 -0400

    Update tetrisGame.py

--- tetrisGame.py
+++ tetrisGame.py
@@ -3,6 +3,7 @@ from board import *
 from tetramino import *
 import configs
 import random
+import copy
 
 """
 This will be the object created for an instance of the game
@@ -10,7 +11,9 @@ This will be the object created for an instance of the game
 
 class Tetris:
     def __init__(self, x: int, y: int) -> None:
-        this.x = x
-        this.y = y
-        
-    
+        self.x = x
+        self.y = y
+        self.colors = copy.deepcopy(configs.colorTiles)
+
+    def setColorScheme(self, colors: List[pygame.image]) -> None:
+        this.colors = colors
