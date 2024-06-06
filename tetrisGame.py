ca78333659b3ff02736f05b509695651466191d3
Arjun Rangarajan <arjunrangarajan2008@gmail.com>
2024-06-05 21:49:06 -0400

    Added level display

--- tetrisGame.py
+++ tetrisGame.py
@@ -127,3 +127,10 @@ class Tetris:
         pygame.draw.rect(surface, (0,0,0), pygame.Rect((self.x+(self.cols+1)*28+14, self.y+14, 200-28, 78-28)))
         text = Tetris.font.render(str(self.score), False, (255,255,255))
         surface.blit(text, (self.x+(self.cols+1)*28+14+5, self.y+14+5))
+
+        ##level display
+
+        pygame.draw.rect(surface, (127, 127, 127), pygame.Rect((self.x + (self.cols + 1) * 28, self.y + self.rows * 56/3, 200, 78)))
+        pygame.draw.rect(surface, (0, 0, 0), pygame.Rect((self.x + (self.cols + 1) * 28 + 14, self.y + self.rows * 56/3 + 14, 200 - 28, 50)))
+        text = Tetris.font.render("Level: " + str(self.level + 1), False, (255, 255, 255))
+        surface.blit(text, (self.x + (self.cols + 1) * 28 + 14 + 5, self.y + self.rows * 56/3 + 14 + 5))
